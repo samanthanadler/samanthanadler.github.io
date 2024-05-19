268d2ae04236df80ca612ed8f58ffb9c642a7656
Samantha Nadler <149008331+samanthanadler@users.noreply.github.com>
2024-05-19 15:31:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function resizeTopnav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
