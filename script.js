3cf2f9c837499cb69fb310bdad4203118c1fb883
Samantha Nadler <149008331+samanthanadler@users.noreply.github.com>
2024-05-19 15:57:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+window.onscroll = function() {stickTopnav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyTopnav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 function resizeTopnav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
